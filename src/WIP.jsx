1fc714066a739fde577d16e643e5b5cf514a590c
Melody Zhong <m4zhong@uwaterloo.ca>
2026-07-09 00:18:49 -0500

    added alt text

--- src/WIP.jsx
+++ src/WIP.jsx
@@ -3,7 +3,7 @@ import cauldron from './assets/cauldron_poem.png';
 
 function WIP() {
   return (
-      <img src={cauldron} alt="Cauldron Poem" style={{ maxWidth: '100%', height: 'auto', margin: '0 auto' }} />
+      <img src={cauldron} alt="A drawing of a cauldron with the poem: Slip of the narrows rabbit. treads from footprints. ferns & stale bread to sleep where Darkness makes its bed." style={{ maxWidth: '100%', height: 'auto', margin: '0 auto' }} />
   );
 }
 
